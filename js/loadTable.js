4440ba0ee63e8b6303ccfec331662a53dd12a0d2
Astroneko404 <33112694+Astroneko404@users.noreply.github.com>
2021-05-23 01:49:01 +0800

    stage_filter

--- js/loadTable.js
+++ js/loadTable.js
@@ -4,14 +4,18 @@ function appendTable(data) {
     // Loading table from JSON file
     var line;
     for (var i=0; i<data.length; i++) {
-        line = $('<tr/>')
+        line = $('<tr />')
         line.append("<td>" + data[i].Item + "</td>");
-        line.append("<td>" + data[i].Place + "</td>");
+        line.append(
+            '<td class="data-stage" rel="' + data[i].Place +
+            '">' + data[i].Place + "</td>"
+        );
         line.append("<td>" + data[i].Category + "</td>");
         line.append("<td>" + data[i].Level + "</td>");
         line.append("<td>" + data[i].Location + "</td>");
         line.append("<td></td>");
         $('tbody').append(line);
+        // console.log(line);
         // return false; // Test purpose
     }
 
@@ -35,6 +39,30 @@ function picHandlers() {
     currentRow.onclick = createClickHandler(currentRow);
   }
 }
+
+$("input:checkbox").click(function () {
+    var showAll = true;
+    $('tr').not('.first').hide();
+    $('input[type=checkbox]').each(function () {
+        if ($(this)[0].checked) {
+            showAll = false;
+            var status = $(this).attr('rel');
+            var value = $(this).val();
+
+            	$('td').each(function() {
+              	if ($(this).attr('class') == status) {
+                    if ($(this).attr('rel').includes(value)) {
+                        $(this).parent('tr').show();
+                    }
+                }
+              });
+            /* $('td.' + status + '[rel="/.g' + value + '/.g"]').parent('tr').show() */;
+        }
+    });
+    if(showAll){
+        $('tr').show();
+    }
+});
 
 window.onload = function() {
     $.getJSON("data/Katamari Cleaned.json", appendTable);
